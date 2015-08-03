1240d06f2c0bdc953fa15eb7271c4f466bb4f611
AndersHogqvist <anders@hogqvist.se>
2015-08-04 00:52:10 +0200

    Added 'cut_shadow' attribute to ElevationBehaviour Changed NavigationDrawer so that there's no shadow on ToolBar

--- kivymd/elevationbehaviour.py
+++ kivymd/elevationbehaviour.py
@@ -2,13 +2,39 @@
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import (ListProperty, ObjectProperty, NumericProperty)
+from kivy.properties import (ListProperty, ObjectProperty, NumericProperty,
+                             OptionProperty)
 from kivy.properties import AliasProperty
 from kivy.metrics import dp
 
-elevation_kv = '''
-<ElevationBehaviour>
-	canvas.before:
+Builder.load_string('''
+<ElevationBehaviour>:
+	canvas:
+		StencilPush
+		Rectangle:
+			size: self._stencil_size
+			pos: self._stencil_pos
+		StencilUse
+		Color:
+			a: self._soft_shadow_a
+		Rectangle:
+			texture: self._soft_shadow_texture
+			size: self._soft_shadow_size
+			pos: self._soft_shadow_pos
+		Color:
+			a: self._hard_shadow_a
+		Rectangle:
+			texture: self._hard_shadow_texture
+			size: self._hard_shadow_size
+			pos: self._hard_shadow_pos
+		StencilUnUse
+		StencilPop
+		Color:
+			a: 1
+
+
+<RoundElevationBehaviour>:
+	canvas:
 		Color:
 			a: self._soft_shadow_a
 		Rectangle:
@@ -23,13 +49,38 @@ elevation_kv = '''
 			pos: self._hard_shadow_pos
 		Color:
 			a: 1
+''')
 
 
-'''
-Builder.load_string(elevation_kv)
 class ElevationBehaviour(object):
+	""":class:`ElevationBehaviour` is an attempt to implement the 3D look in
+	Google's Material Design. This is not a correct shadow projection, it
+	doesn't care about the elevation of underlying widgets and there it doesn't
+	detect the shape of the widget.
+
+	The shadow effect is made by several images with a black rectangle that's
+	more or less blurred. In Material Design, there's two light sources that
+	casts shadows. Here we have two images, one for the 'soft shadow' with more
+	blur and one for the 'hard shadow' with less blur.
+
+	Since most widgets used in Material Design are either rectangular or
+	circular, we have made two implementations for one of each shape.
+
+	With :attr:`cut_shadow` we can simulate the case of a widget being at the
+	same level as another widget by cropping the shadow at either side. The
+	:class:`NavigationDrawer` for instance, that is on the same 'height' as
+	the :class:`ToolBar`, has this attribute set to 'top' so that no shadow
+	is visible above it.
+
+	..note::
+		:attr:`cut_shadow` only works on widgets with static elevations. If
+		the attr:`elevation` is animated, there's no shadow appearing on the
+		side where it is cropped. That's something we will try to figure out
+		how to implement.
+	"""
 
 	_elevation = NumericProperty(1)
+
 	def _get_elevation(self):
 		return self._elevation
 
@@ -39,8 +90,28 @@ class ElevationBehaviour(object):
 		except:
 			self._elevation = 1
 
-	elevation = AliasProperty(_get_elevation, _set_elevation, bind=('_elevation', ))
+	elevation = AliasProperty(_get_elevation, _set_elevation,
+	                          bind=('_elevation',))
+	"""By increasing the :attr:`elevation` of a widget the shadow become larger
+	 and 'softer', making it appear more or less elevated from its background.
 
+	This can be animated to have the widget 'moving' on the z-axis.
+
+	:attr:`elevation` is a :class:`~kivy.properties.AliasPropery` and
+	defaults to 1
+	"""
+
+	cut_shadow = OptionProperty('no_cut', options=('no_cut', 'top',
+	                                               'right', 'bottom',
+	                                               'left'))
+	"""The attr:`cut_shadow` can be set to 'no_cut', 'top', 'right', 'bottom',
+	or 'left' in order to remove the shadow from either side. This can be used
+	when you want the widget to appear on the same 'height' as a widget next to
+	it.
+
+	:attr:`determinate` is a :class:`~kivy.properties.OptionProperty` and
+	defaults to 'no_cut'.
+	"""
 
 	_soft_shadow_texture = ObjectProperty()
 	_soft_shadow_size = ListProperty([0, 0])
@@ -50,85 +121,96 @@ class ElevationBehaviour(object):
 	_hard_shadow_size = ListProperty([0, 0])
 	_hard_shadow_pos = ListProperty([0, 0])
 	_hard_shadow_a = NumericProperty(0)
+	_stencil_size = ListProperty([0, 0])
+	_stencil_pos = ListProperty([0, 0])
+
 	def __init__(self, **kwargs):
 		super(ElevationBehaviour, self).__init__(**kwargs)
 		self.bind(elevation=self._update_shadow,
-				  pos=self._update_shadow,
-				  size=self._update_shadow)
+		          pos=self._update_shadow,
+		          size=self._update_shadow,
+		          cut_shadow=self._update_shadow)
 
 	def _update_shadow(self, *args):
 		if self.elevation > 0:
 			ratio = self.width / self.height
 			if ratio > -2 and ratio < 2:
 				self._shadow = App.get_running_app().theme_cls.quad_shadow
-				width = self.width * 1.9
-				height = self.height * 1.9
+				width = soft_width = self.width * 1.9
+				height = soft_height = self.height * 1.9
 			elif ratio <= -2:
 				self._shadow = App.get_running_app().theme_cls.rec_st_shadow
 				ratio = abs(ratio)
-				if ratio > 5 :
-					ratio = ratio*22
+				if ratio > 5:
+					ratio = ratio * 22
 				else:
-					ratio = ratio*11.5
+					ratio = ratio * 11.5
 
-				width = self.width * 1.9
+				width = soft_width = self.width * 1.9
 				height = self.height + dp(ratio)
+				soft_height = self.height + dp(ratio) + dp(self.elevation) * .5
 			else:
 				self._shadow = App.get_running_app().theme_cls.rec_shadow
 				ratio = abs(ratio)
-				if ratio > 5 :
-					ratio = ratio*22
+				if ratio > 5:
+					ratio = ratio * 22
 				else:
-					ratio = ratio*11.5
+					ratio = ratio * 11.5
 
 				width = self.width + dp(ratio)
-				height = self.height * 1.9
+				soft_width = self.width + dp(ratio) + dp(self.elevation) * .9
+				height = soft_height = self.height * 1.9
 
-			x = self.center_x - width/2
-			self._soft_shadow_size = (width, height)
+			x = self.center_x - width / 2
+			soft_x = self.center_x - soft_width / 2
+			self._soft_shadow_size = (soft_width, soft_height)
 			self._hard_shadow_size = (width, height)
 
+			y = self.center_y - soft_height / 2 - dp(
+				.1 * 1.5 ** self.elevation)
+			self._soft_shadow_pos = (soft_x, y)
+			self._soft_shadow_a = 0.1 * 1.1 ** self.elevation
+			self._soft_shadow_texture = self._shadow.textures[
+				str(int(round(self.elevation - 1)))]
 
-			y = self.center_y - height/2 - dp(.1 * 1.5**self.elevation)
-			self._soft_shadow_pos = (x, y)
-			self._soft_shadow_a = 0.1 * 1.11**self.elevation
-			self._soft_shadow_texture = self._shadow.textures[str(int(round(self.elevation*2 -1)))]
-
-			y = self.center_y - height/2 - dp(.5 * 1.2**self.elevation)
+			y = self.center_y - height / 2 - dp(.5 * 1.18 ** self.elevation)
 			self._hard_shadow_pos = (x, y)
-			self._hard_shadow_a = .5 * .88**self.elevation
-			self._hard_shadow_texture = self._shadow.textures[str(int(round(self.elevation-1)))]
+			self._hard_shadow_a = .4 * .9 ** self.elevation
+			self._hard_shadow_texture = self._shadow.textures[
+				str(int(round(self.elevation)))]
+
+			if self.cut_shadow == 'no_cut':
+				self._stencil_size = self._soft_shadow_size
+				self._stencil_pos = self._soft_shadow_pos
+			elif self.cut_shadow == 'top':
+				self._stencil_size = (self._soft_shadow_size[0] + self.width,
+				                      self._soft_shadow_size[1] + self.height)
+				self._stencil_pos = (self._soft_shadow_pos[0],
+				                     self.y - self._soft_shadow_size[1])
+			elif self.cut_shadow == 'right':
+				self._stencil_size = (self._soft_shadow_size[0] + self.width,
+				                      self._soft_shadow_size[1] + self.height)
+				self._stencil_pos = (self.x - self._soft_shadow_size[0],
+				                     self._soft_shadow_pos[1])
+			elif self.cut_shadow == 'bottom':
+				self._stencil_size = (self._soft_shadow_size[0] + self.width,
+				                      self._soft_shadow_size[1] + self.height)
+				self._stencil_pos = (self._soft_shadow_pos[0],
+				                     self.y)
+			elif self.cut_shadow == 'left':
+				self._stencil_size = (self._soft_shadow_size[0] + self.width,
+				                      self._soft_shadow_size[1] + self.height)
+				self._stencil_pos = (self.x,
+				                     self._soft_shadow_pos[1])
 
 		else:
 			self._soft_shadow_a = 0
 			self._hard_shadow_a = 0
 
 
-
-round_elevation_kv = '''
-<RoundElevationBehaviour>
-	canvas.before:
-		Color:
-			a: self._soft_shadow_a
-		Rectangle:
-			texture: self._soft_shadow_texture
-			size: self._soft_shadow_size
-			pos: self._soft_shadow_pos
-		Color:
-			a: self._hard_shadow_a
-		Rectangle:
-			texture: self._hard_shadow_texture
-			size: self._hard_shadow_size
-			pos: self._hard_shadow_pos
-		Color:
-			a: 1
-
-
-'''
-Builder.load_string(round_elevation_kv)
 class RoundElevationBehaviour(object):
-
 	_elevation = NumericProperty(1)
+
 	def _get_elevation(self):
 		return self._elevation
 
@@ -138,8 +220,8 @@ class RoundElevationBehaviour(object):
 		except:
 			self._elevation = 1
 
-	elevation = AliasProperty(_get_elevation, _set_elevation, bind=('_elevation', ))
-
+	elevation = AliasProperty(_get_elevation, _set_elevation,
+	                          bind=('_elevation',))
 
 	_soft_shadow_texture = ObjectProperty()
 	_soft_shadow_size = ListProperty([0, 0])
@@ -149,32 +231,35 @@ class RoundElevationBehaviour(object):
 	_hard_shadow_size = ListProperty([0, 0])
 	_hard_shadow_pos = ListProperty([0, 0])
 	_hard_shadow_a = NumericProperty(0)
+
 	def __init__(self, **kwargs):
 		super(RoundElevationBehaviour, self).__init__(**kwargs)
 		self._shadow = App.get_running_app().theme_cls.round_shadow
 		self.bind(elevation=self._update_shadow,
-				  pos=self._update_shadow,
-				  size=self._update_shadow)
+		          pos=self._update_shadow,
+		          size=self._update_shadow)
 
 	def _update_shadow(self, *args):
 		if self.elevation > 0:
 			width = self.width * 2
 			height = self.height * 2
 
-			x = self.center_x - width/2
+			x = self.center_x - width / 2
 			self._soft_shadow_size = (width, height)
 
 			self._hard_shadow_size = (width, height)
 
-			y = self.center_y - height/2 - dp(.1 * 1.5**self.elevation)
+			y = self.center_y - height / 2 - dp(.1 * 1.5 ** self.elevation)
 			self._soft_shadow_pos = (x, y)
-			self._soft_shadow_a = 0.1 * 1.11**self.elevation
-			self._soft_shadow_texture = self._shadow.textures[str(int(round(self.elevation*2 -1)))]
+			self._soft_shadow_a = 0.1 * 1.1 ** self.elevation
+			self._soft_shadow_texture = self._shadow.textures[
+				str(int(round(self.elevation)))]
 
-			y = self.center_y - height/2 - dp(.5 * 1.2**self.elevation)
+			y = self.center_y - height / 2 - dp(.5 * 1.18 ** self.elevation)
 			self._hard_shadow_pos = (x, y)
-			self._hard_shadow_a = .5 * .88**self.elevation
-			self._hard_shadow_texture = self._shadow.textures[str(int(round(self.elevation-1)))]
+			self._hard_shadow_a = .4 * .9 ** self.elevation
+			self._hard_shadow_texture = self._shadow.textures[
+				str(int(round(self.elevation - 1)))]
 
 		else:
 			self._soft_shadow_a = 0
---
+++ kivymd/navigationdrawer.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from kivy.clock import Clock
+from kivy.lang import Builder
+from kivy.metrics import dp
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.relativelayout import RelativeLayout
+from kivymd.elevationbehaviour import ElevationBehaviour
+from kivymd.icon_definitions import md_icons
+from kivymd.label import MaterialLabel
+from kivymd.list import MaterialList, ListItem, LeftListItemBody
+from kivymd.slidingpanel import SlidingPanel
+from kivymd.theming import ThemableBehavior
+
+
+Builder.load_string('''
+<NavigationDrawer>
+	_list: _list
+	cut_shadow: 'top'
+	canvas:
+		Color:
+			rgba: root.theme_cls.bg_light
+		Rectangle:
+			size: root.size
+			pos: root.pos
+	Image:
+		id: _header_bg
+		size_hint_y: None
+		height: 0 if self.source == '' or self.source == None else 9 * self.width / 16
+		x: root.x
+		y: root.height - self.height
+		mipmap: True
+		allow_stretch: True
+		keep_ratio: False
+	ScrollView:
+		size_hint_y: None
+		height: root.height - _header_bg.height - dp(8)
+		pos: root.pos
+		MaterialList:
+			id: _list
+''')
+
+
+class NavigationDrawer(SlidingPanel, ThemableBehavior, ElevationBehaviour):
+	'''Implementation of the Navigation Drawer pattern.'''
+
+	header_img = StringProperty()
+
+	_list = ObjectProperty()
+	_header_bg = ObjectProperty()
+
+	def add_widget(self, widget, index=0):
+		if issubclass(widget.__class__, NavigationDrawerCategory):
+			self._list.add_widget(widget, index)
+		else:
+			super(NavigationDrawer, self).add_widget(widget, index)
+
+	def on_header_img(self, instance, value):
+		self._header_bg.source = value
+
+
+class NavigationDrawerCategory(MaterialList):
+
+	def __init__(self, **kwargs):
+		super(NavigationDrawerCategory, self).__init__(**kwargs)
+		self.padding = (0, self.padding[1], 0, 0)
+
+
+class NavigationDrawerButton(ListItem):
+
+	icon = StringProperty()
+
+	def __init__(self, **kwargs):
+		super(NavigationDrawerButton, self).__init__(**kwargs)
+		self.lbl_icon = LeftIcon(font_style='Icon',
+		                         theme_text_color='Custom',
+		                         text_color=(0,0,0,0.54))
+		Clock.schedule_once(self.initialization_instructions)
+
+	def initialization_instructions(self, _):
+		self.add_widget(self.lbl_icon)
+		self.on_icon(None, self.icon)
+
+	def on_icon(self, instance, value):
+		if value == '':
+			self.left_container_size = None
+		else:
+			self.left_container_size = 'small'
+			self.lbl_icon.text = u"{}".format(md_icons[value])
+
+	def on_parent(self, instance, value):
+		if not issubclass(value.__class__, NavigationDrawerCategory):
+			raise Exception("NavigationDrawerButton may only be placed inside"
+			                " a NavigationDrawerCategory widget")
+
+class LeftIcon(LeftListItemBody, MaterialLabel):
+	pass
