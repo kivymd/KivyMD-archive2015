36103c5642f79f92d26209800c9eaa7e27c67b11
Andrés Rodríguez <andres.rodriguez@lithersoft.com>
2016-02-06 18:20:40 -0300

    Deprecated GitHub's version of KivyMD, added message

--- kivymd/__init__.py
+++ kivymd/__init__.py
@@ -4,3 +4,22 @@ import os
 path = os.path.dirname(__file__)
 fonts_path = os.path.join(path, "fonts/")
 images_path = os.path.join(path, 'images/')
+
+migration_msg = '''
+#############################################
+#                                           #
+# KivyMD has moved to GitLab, this version  #
+# has been deprecated.                      #
+#                                           #
+# Find the new repository at:               #
+# https://gitlab.com/kivymd/KivyMD          #
+#                                           #
+# Reasoning for migration found at old repo #
+#                                           #
+#############################################
+'''
+
+class KivyMDHasMovedToGitLab(Exception):
+	pass
+
+raise KivyMDHasMovedToGitLab(migration_msg)
